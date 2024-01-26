98a11238c4eed3be4d52cd75f725577a474c2812
Maddi <maddi.fitzmaurice@gmail.com>
2024-01-26 12:19:17 +1100

    #25 SoulsManager A
    - Started SoulsManager
    - Added more functionality to Soul
    - Added a SoulGrouper

--- Assets/Scripts/Interactables/Soul.cs
+++ Assets/Scripts/Interactables/Soul.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Soul : MonoBehaviour
 {
+    [Header("Script")]
     [SerializeField] private TextAsset _soulText;
+
+    public Judging Status { get; private set; } = Judging.UNJUDGED;
+
+    public void SetStatus(Judging status)
+    {
+        Status = status;
+    }
 }
--- Assets/Scripts/Managers/SoulsManager.cs
+++ Assets/Scripts/Managers/SoulsManager.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+
+public enum Judging
+{
+    UNJUDGED,
+    HEAVEN,
+    HELL
+}
 
 public class SoulsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Souls Parent Object")]
+    [SerializeField] private GameObject _soulsParent;
+
+    private List<Soul> _souls;
+
+    private void Awake()
     {
-        
+        // Collect all souls and make into list
+        _souls = _soulsParent.GetComponentsInChildren<Soul>().ToList<Soul>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool IsFinishedJudging()
     {
-        
+        foreach (Soul soul in _souls)
+        {
+            if (soul.Status == Judging.UNJUDGED)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
